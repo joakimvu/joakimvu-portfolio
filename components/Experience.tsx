6efc83e1635d889d2a89e4273fb2d2fdfde05359
Joakim Vu <joakimvu@icloud.com>
2024-10-02 16:14:50 +0200

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,4 +1,6 @@
+import { workExperience } from "@/data";
 import React from "react";
+import { Button } from "./ui/MovingBorders";
 
 const Experience = () => {
   return (
@@ -7,7 +9,30 @@ const Experience = () => {
         Mine
         <span className="text-purple"> arbeidserfaringer</span>
       </h1>
-      <div className="flex flex-col items-center max-lg:mt-10">a a a a</div>
+      <div className="w-full mt-12 grid lg:cols-4 grid-cols-1 gap-10">
+        {workExperience.map((card) => (
+          <Button
+            key={card.id}
+            borderRadius="1.75rem"
+            className="flex-1 text-wrap border-neutral-200 dark:border-slate-800"
+            duration={Math.floor(Math.random() * 10000) + 10000}
+          >
+            <div className="flex lg:flex-row flex-col items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+              <img
+                src={card.thumbnail}
+                alt={card.thumbnail}
+                className="lg:w-32 m:w-20 w-16"
+              />
+              <div className="lg:ms5">
+                <h1 className="text-start text-xl md:text-2xl">{card.title}</h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {card.desc}
+                </p>
+              </div>
+            </div>
+          </Button>
+        ))}
+      </div>
     </div>
   );
 };
