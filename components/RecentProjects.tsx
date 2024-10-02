0418cd15e1bae9a300727482a195cc18eed17e87
Joakim Vu <joakimvu@icloud.com>
2024-10-02 10:00:45 +0200

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -9,14 +9,14 @@ const RecentProjects = () => {
       <h1 className="heading">
         Et lite utvalg av <span className="text-purple">nylige prosjekter</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 mt-10">
-        {projects.map(({ id, title, desc, img, iconLists, link }) => (
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
           >
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg-[30vh] mb-10">
+              <div className="relative flex items-center sm:w-[570px] w-[80vw] overflow-hidden sm:g-[40vw] h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
@@ -26,7 +26,7 @@ const RecentProjects = () => {
                 {title}
               </h1>
               <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
-                {desc}
+                {des}
               </p>
               <div className="flex item-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
