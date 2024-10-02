e247040c68071c33a6f7010a60a48c6b3c767834
Joakim Vu <joakimvu@icloud.com>
2024-10-02 15:20:19 +0200

    Create Experience.tsx

---
+++ components/Experience.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="py-20" id="contact">
+      <h1 className="heading">
+        Mine
+        <span className="text-purple"> arbeidserfaringer</span>
+      </h1>
+      <div className="flex flex-col items-center max-lg:mt-10">a a a a</div>
+    </div>
+  );
+};
+
+export default Experience;
