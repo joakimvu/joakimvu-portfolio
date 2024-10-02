7c320c758843ebf626a3e784c163e315ce278a34
Joakim Vu <joakimvu@icloud.com>
2024-10-02 16:39:40 +0200

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -14,6 +15,7 @@ export default function Home() {
         <Grid />
         <RecentProjects /> {/*TODO: Fikse hydration error */}
         <Experience />
+        <Footer />
       </div>
     </main>
   );
