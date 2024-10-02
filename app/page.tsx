13ffe685b26a25383ff90e00cf5cfbab68d33651
Joakim Vu <joakimvu@icloud.com>
2024-10-02 15:20:34 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -11,7 +12,8 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-        <RecentProjects />
+        <RecentProjects /> {/*TODO: Fikse hydration error */}
+        <Experience />
       </div>
     </main>
   );
