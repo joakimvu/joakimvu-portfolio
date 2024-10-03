a0e64d76a1e7ef6d9ba996d9b8a23e3befc1b65d
Joakim Vu <joakimvu@icloud.com>
2024-10-03 21:58:15 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,29 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <Hero />
-        <Grid />
-        <RecentProjects />
-        <Experience />
-        <Footer />
+    <div>
+      <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <FloatingNav navItems={navItems} />
+          <Hero />
+          <Grid />
+        </div>
       </div>
-    </main>
+      <div className="relative bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <RecentProjects />
+        </div>
+      </div>
+      <div className="relative bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <Experience />
+        </div>
+      </div>
+      <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <Footer />
+        </div>
+      </div>
+    </div>
   );
 }
