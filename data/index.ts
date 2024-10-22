8f7df6b5f71a01c1b059177fe5ec0059cf0a1cf7
Joakim Vu <joakimvu@icloud.com>
2024-10-22 16:55:25 +0200

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,8 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
+  { name: "Om meg", link: "#about" },
+  { name: "Prosjekter", link: "#projects" },
   // { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+  { name: "Kontakt", link: "#contact" },
 ];
 
 export const gridItems = [
