f226350a40560f45816b80c8aa900749eb471fcf
Joakim Vu <joakimvu@icloud.com>
2024-10-29 13:45:09 +0100

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -15,12 +15,12 @@ const RecentProjects = () => {
         <span className="text-pink-700"> nylige prosjekter</span>
       </h1>
 
-      <div className="flex flex-wrap mt-8 gap-8">
+      <div className="flex flex-wrap mt-12 gap-12">
         {projects.map(({ id, title, des, img, link, techList }) => (
           <div
             key={id}
             className={cn(
-              "w-full md:w-[calc(50%-16px)] p-4 flex flex-col items-start bg-white-100 bg-transparent  overflow-hidden shadow-[0_8px_16px_rgb(0_0_0/0.4)] rounded-2xl border border-white/[0.1]"
+              "w-full md:w-[calc(50%-28px)] p-4 flex flex-col items-start bg-white-100 bg-transparent  overflow-hidden shadow-[0_8px_16px_rgb(0_0_0/0.4)] rounded-2xl border border-white/[0.1]"
             )}
           >
             <Image src={img} alt={img} className="" width={1000} height={100} />
