4184dbb0233d4ae4e9f69215d772265e800201e3
Joakim Vu <joakimvu@icloud.com>
2024-10-30 18:43:38 +0100

    fix themetoggle on small device

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -3,9 +3,11 @@
 import { useEffect, useState } from "react";
 import { FaMoon } from "react-icons/fa";
 import { BsSunFill } from "react-icons/bs";
+import { useMediaQuery } from "react-responsive";
 
 const ThemeToggle = () => {
   const [darkMode, setDarkMode] = useState(true);
+  const [isMounted, setIsMounted] = useState(false); // New state to track mount status
 
   useEffect(() => {
     const theme = localStorage.getItem("theme");
@@ -22,23 +24,46 @@ const ThemeToggle = () => {
     }
   }, [darkMode]);
 
+  // Use react-responsive to check if the screen width is at least 768px
+  const isLargeDevice = useMediaQuery({ query: "(min-width: 768px)" });
+
+  // Set the mounted state to true after the component has mounted
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null; // Render nothing until the component is mounted
+  }
+
   return (
-    <div
-      className="relative w-16 h-8 flex items-center dark:bg-gray-900 bg-white-200 cursor-pointer rounded-full p-1"
-      onClick={() => setDarkMode(!darkMode)}
-    >
-      <FaMoon className="text-white" size={18} />
-      {darkMode ? (
-        <div className="text-white"></div> // Optional text for dark mode
+    <div>
+      {isLargeDevice ? (
+        <div
+          className="relative w-16 h-8 flex items-center dark:bg-gray-900 bg-white-200 cursor-pointer rounded-full p-1"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          <FaMoon className="text-white" size={18} />
+          <div
+            className={`absolute bg-white dark:bg-gray-700 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 ${
+              darkMode ? "translate-x-0" : "translate-x-8"
+            }`}
+          ></div>
+          <BsSunFill className="ml-auto text-yellow-400" size={18} />
+        </div>
       ) : (
-        <div className="text-gray-800"></div> // Optional text for light mode
+        <div
+          className="relative w-7 h-7 flex items-center dark:bg-gray-900 bg-white-200 cursor-pointer rounded-full p-1"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          {/* Content for small devices */}
+          {darkMode ? (
+            <BsSunFill className="ml-auto text-yellow-400" size={18} />
+          ) : (
+            <FaMoon className="text-white" size={18} />
+          )}
+        </div>
       )}
-      <div
-        className={`absolute bg-white dark:bg-gray-700 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 ${
-          darkMode ? "translate-x-0" : "translate-x-8"
-        }`}
-      ></div>
-      <BsSunFill className="ml-auto text-yellow-400" size={18} />
     </div>
   );
 };
